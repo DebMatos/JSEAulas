19b4ee091c9801c2195e44c490ee0fa899ea03f5
DebMatos <matos.debora88@gmail.com>
2018-11-27 17:49:21 +0000

    j

--- StateMachine/src/io/altar/states/MenuInicial.java
+++
@@ -1,5 +0,0 @@
-package io.altar.stateMachine.states;
-
-public class MenuInicial {
-
-}
--- StateMachine/src/io/altar/states/State.java
+++
@@ -1,9 +0,0 @@
-package io.altar.stateMachine.states;
-
-import io.altar.stateMachine.utils.ScannerUtils;
-
-public interface State {
-	public static final ScannerUtils scannerUtils = new ScannerUtils();
-
-	public int execute();
-}
